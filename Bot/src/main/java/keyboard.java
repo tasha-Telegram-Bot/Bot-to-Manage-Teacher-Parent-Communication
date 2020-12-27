3c54ef7ddbedc6348d19fe3adb4afc3514a097e3
AchaaHashim <74227418+AchaaHashim@users.noreply.github.com>
2020-12-27 13:11:19 +0800

    Add files via upload

---
+++ Bot/src/main/java/keyboard.java
@@ -0,0 +1,38 @@
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class keyboard {
+    public synchronized void setButtons(SendMessage sendMessage) {
+
+        // Create a keyboard
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Create a list of keyboard rows
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        // First keyboard row
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        // Add buttons to the first keyboard row
+        keyboardFirstRow.add(new KeyboardButton("Hi"));
+
+        // Second keyboard row
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Add the buttons to the second keyboard row
+        keyboardSecondRow.add(new KeyboardButton("Help"));
+
+        // Add all of the keyboard rows to the list
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        // and assign this list to our keyboard
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+}
